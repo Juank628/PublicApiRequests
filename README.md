f6f18d3cf6af396323045a07ab20a7cb681b6c05
Juank628 <jsanchez_zunino@yahoo.com>
2019-07-01 18:34:35 -0500

    exceeds expectations

---
+++ README.md
@@ -0,0 +1,3 @@
+box shadow added to card class
+:hover pseudo-class added to card class, to incress the size when the pointer is over
+:hover pseudo-class added to modal buttons, to change backgrond color to green wuen the mouse is over
--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,79 @@
 /*global variables*/
 let allUsers = [];
 
+/*update modal data when prev next and buttons are clicked*/
+const changeModal = (direction) => {
+    currentIndex = 0
+    const currentName = document.querySelector('.modal-name').textContent;
+    allUsers.forEach((user, index) => {
+        if((`${user.name.first} ${user.name.last}`) === currentName){
+            currentIndex = index
+        }
+    })
+
+    let newUserIndex = currentIndex + direction
+    if(newUserIndex < 0){
+        newUserIndex = 0
+    }
+    if(newUserIndex > 11){
+        newUserIndex = 11
+    }
+
+    const newUser = allUsers[newUserIndex]
+    document.querySelectorAll('.modal-img')[0].src = newUser.picture.large
+    document.querySelectorAll('.modal-name')[0].textContent = `${newUser.name.first} ${newUser.name.last}`
+    document.querySelectorAll('.modal-text')[0].textContent = newUser.email
+    document.querySelectorAll('.modal-text')[1].textContent = newUser.location.city
+    document.querySelectorAll('.modal-text')[2].textContent = newUser.cell
+    document.querySelectorAll('.modal-text')[3].textContent = `${newUser.location.street}, ${newUser.location.state} ${newUser.location.postcode}`
+    document.querySelectorAll('.modal-text')[4].textContent = `${getBirthday(newUser.dob.date)}`
+}
+
+/*format birthday according the mockup*/
 const getBirthday = date => {
-    birthdayData = date.slice(0,10).split('-');
-    return `${birthdayData[1]}/${birthdayData[2]}/${birthdayData[0].slice(2,4)}`
-}
+  birthdayData = date.slice(0, 10).split("-");
+  return `${birthdayData[1]}/${birthdayData[2]}/${birthdayData[0].slice(2, 4)}`;
+};
 
+/*remove al modal elements when the modal is closed*/
 const closeModal = () => {
-    document.querySelector('.modal-container').remove();
-}
+  document.querySelector(".modal-container").remove();
+};
 
-const openModal = (uuid) => {
-    userData = allUsers.filter(user => user.login.uuid === uuid)[0];
-    console.log(userData)
-    const modalDiv = document.createElement('div');
-    modalDiv.className = "modal-container"
-    modalDiv.innerHTML = 
-        `<div class="modal">
-            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-            <div class="modal-info-container">
-                <img class="modal-img" src=${userData.picture.large} alt="profile picture">
-                <h3 id="name" class="modal-name cap">${userData.name.first} ${userData.name.last}</h3>
-                <p class="modal-text">${userData.email}</p>
-                <p class="modal-text cap">${userData.location.city}</p>
-                <hr>
-                <p class="modal-text">${userData.cell}</p>
-                <p class="modal-text">${userData.location.street}, ${userData.location.state} ${userData.location.postcode}</p>
-                <p class="modal-text">Birthday: ${getBirthday(userData.dob.date)}</p>
+/*open the modal and load all the data according de user uuid*/
+const openModal = uuid => {
+  userData = allUsers.filter(user => user.login.uuid === uuid)[0];
+  const modalDiv = document.createElement("div");
+  modalDiv.className = "modal-container";
+  modalDiv.innerHTML = `
+            <div class="modal">
+                <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                <div class="modal-info-container">
+                    <img class="modal-img" src=${userData.picture.large} alt="profile picture">
+                    <h3 id="name" class="modal-name cap">${userData.name.first} ${userData.name.last}</h3>
+                    <p class="modal-text">${userData.email}</p>
+                    <p class="modal-text cap">${userData.location.city}</p>
+                    <hr>
+                    <p class="modal-text">${userData.cell}</p>
+                    <p class="modal-text">${userData.location.street}, ${userData.location.state} ${userData.location.postcode}</p>
+                    <p class="modal-text">Birthday: ${getBirthday(userData.dob.date)}</p>
+                </div>
             </div>
-        </div>
+            <div class="modal-btn-container">
+                <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+                <button type="button" id="modal-next" class="modal-next btn">Next</button>
+            </div>`;
+  document.body.appendChild(modalDiv);
+  document.getElementById("modal-close-btn").addEventListener("click", () => closeModal());
+  document.getElementById("modal-prev").addEventListener('click', ()=>changeModal(-1));
+  document.getElementById("modal-next").addEventListener('click', ()=>changeModal(+1));
+};
 
-        <div class="modal-btn-container">
-            <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
-            <button type="button" id="modal-next" class="modal-next btn">Next</button>
-        </div>`;
-    document.body.appendChild(modalDiv)
-    document.getElementById('modal-close-btn').addEventListener('click', () => closeModal())
-}
-
-const generateCards = (users) => {
-    let html = users.results.map(user => 
-        `<div class="card" id=${user.login.uuid}>
+/*render the user cards and load its data*/
+const generateCards = users => {
+  let html = users.map(
+    user =>
+      `<div class="card" id=${user.login.uuid}>
             <div class="card-img-container">
                 <img class="card-img" src="${user.picture.medium}" alt="profile picture">
             </div>
@@ -50,18 +83,51 @@ const generateCards = (users) => {
                 <p class="card-text cap">${user.location.city}, ${user.location.state}</p>
             </div>
         </div>`
-    )
-    document.getElementById('gallery').innerHTML = html.join('')
-    allUsers = users.results
-    return Promise.resolve('All cards added')
-}
+  );
+  document.getElementById("gallery").innerHTML = html.join("");
+  return Promise.resolve("All cards added");
+};
 
-const generateEvents = () => {
-    const cards = document.querySelectorAll('.card')
-    cards.forEach(card => card.addEventListener('click',()=>openModal(card.id)))
-    return Promise.resolve('All events added')
-}
+/*add event listener to each card*/
+const generateCardEvents = () => {
+  const cards = document.querySelectorAll(".card");
+  cards.forEach(card =>card.addEventListener("click", () => openModal(card.id)));
+  return Promise.resolve("All events added");
+};
 
+/*filter cards according the search input*/
+const search = name => {
+  const cards = document.querySelectorAll(".card");
+  cards.forEach(card => card.remove());
+  searchUsers = allUsers.filter(user =>
+    user.name.first.toLowerCase().includes(name.toLowerCase()) ||
+    user.name.last.toLowerCase().includes(name.toLowerCase())
+  );
+  generateCards(searchUsers);
+  generateCardEvents();
+};
+
+
+/*Add search bar and search events when the page is loaded*/
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector(".search-container").innerHTML = `
+          <form action="#" method="get">
+              <input type="search" id="search-input" class="search-input" placeholder="Search...">
+              <input type="submit" value="&#x1F50D;" id="serach-submit" class="search-submit">
+          </form>
+          `;
+  
+    document.querySelector("form").addEventListener("submit", e => {
+      e.preventDefault();
+      search(document.querySelector(".search-input").value);
+    });
+  });
 
 /*API Fetch*/
-fetch('https://randomuser.me/api/?results=12').then(res => res.json()).then(generateCards).then(generateEvents);
+fetch("https://randomuser.me/api/?results=12&nat=us,au,ca,gb")
+  .then(res => res.json())
+  .then(data => data.results)
+  .then(users => (allUsers = users))
+  .then(generateCards)
+  .then(generateCardEvents)
+  .catch(err => console.log(err));
